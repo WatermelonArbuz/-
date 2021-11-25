4904133876c7d5f7bb8fb63f61cbae33bdc2926f
WatermelonArbuz <94833658+WatermelonArbuz@users.noreply.github.com>
2021-11-25 13:43:31 +0300

    Add files via upload

---
+++ курсач/Курсач/Modules/Simulation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Курсач.Modules
+{
+    public class Simulation
+
+    {
+        private bool _isActive = false;
+        public Label label;
+        public void Start()
+        {
+           
+            _isActive = true;
+            Task.Run(() => BusinessLoop());
+        }
+        private void BusinessLoop()
+        {
+            while (_isActive) {
+                label.Invoke(new Action(() => label.Text = "dfsdf"));
+                Thread.Sleep(200);
+            }
+        }
+        public void Stop()
+        {
+            _isActive = false; 
+        }
+    }
+}
